7fcb8d30ac5dfd1ddf01dbd3d6e2bd6ee8a8f524
mohsen205 <mohsenmnassri7@gmail.com>
2023-04-20 19:07:41 +0200

    back to old code

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-// const http = require("node:http");
+const http = require("node:http");
 
-// const server = http.createServer((request, response) => {
-//   response.statusCode = 200;
-//   response.setHeader("Content-Type", "application/json");
-//   const data = JSON.stringify({ message: "Hello, world!" });
-//   response.end(data);
-// });
-// const port = process.env.PORT || 5000;
+const server = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader("Content-Type", "application/json");
+  const data = JSON.stringify({ message: "Hello, world!" });
+  response.end(data);
+});
+const port = process.env.PORT || 5000;
 
-// server.listen(port);
+server.listen(port);
 
 // https://learn-node-azure.azurewebsites.net/
 
